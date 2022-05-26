12eacc8e99c8016caffaa3b479628b8aeb5e5a4c
Andrew Banagas <andrewbanagas40@gmail.com>
2022-05-25 19:21:36 -0700

    first commit

---
+++ controllers/api/user-routes.js
@@ -0,0 +1,146 @@
+const router = require("express").Router();
+const { User, Post, Comment } = require("../../models");
+
+// GET user
+router.get("/", (req, res) => {
+  User.findAll({
+    attributes: { exclude: ["[password]"] },
+  })
+    .then((dbUserData) => res.json(dbUserData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+// POST creates a user
+router.post("/", (req, res) => {
+  User.create({
+    username: req.body.username,
+    password: req.body.password,
+  })
+    .then((dbUserData) => {
+      req.session.save(() => {
+        req.session.user_id = dbUserData.id;
+        req.session.username = dbUserData.username;
+        req.session.loggedIn = true;
+
+        res.json(dbUserData);
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+// POST find the user
+router.post("/login", (req, res) => {
+  User.findOne({
+    where: {
+      username: req.body.username,
+    },
+  }).then((dbUserData) => {
+    if (!dbUserData) {
+      res.status(400).json({ message: "No user with that username" });
+      return;
+    }
+
+    const validPassword = dbUserData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res.status(400).json({ message: "Incorrect password!" });
+      return;
+    }
+
+    req.session.save(() => {
+      req.session.user_id = dbUserData.id;
+      req.session.username = dbUserData.username;
+      req.session.loggedIn = true;
+
+      res.json({ user: dbUserData, message: "You are now logged in!" });
+    });
+  });
+});
+
+// POST Log Out
+router.post("/logout", (req, res) => {
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
+
+// // CREATE new user
+// router.post('/', async (req, res) => {
+//   try {
+//     const dbUserData = await User.create({
+//       username: req.body.username,
+//       email: req.body.email,
+//       password: req.body.password,
+//     });
+
+//     req.session.save(() => {
+//       req.session.loggedIn = true;
+
+//       res.status(200).json(dbUserData);
+//     });
+//   } catch (err) {
+//     console.log(err);
+//     res.status(500).json(err);
+//   }
+// });
+
+// // Login
+// router.post('/login', async (req, res) => {
+//   try {
+//     const dbUserData = await User.findOne({
+//       where: {
+//         email: req.body.email,
+//       },
+//     });
+
+//     if (!dbUserData) {
+//       res
+//         .status(400)
+//         .json({ message: 'Incorrect email or password. Please try again!' });
+//       return;
+//     }
+
+//     const validPassword = await dbUserData.checkPassword(req.body.password);
+
+//     if (!validPassword) {
+//       res
+//         .status(400)
+//         .json({ message: 'Incorrect email or password. Please try again!' });
+//       return;
+//     }
+
+//     req.session.save(() => {
+//       req.session.loggedIn = true;
+
+//       res
+//         .status(200)
+//         .json({ user: dbUserData, message: 'You are now logged in!' });
+//     });
+//   } catch (err) {
+//     console.log(err);
+//     res.status(500).json(err);
+//   }
+// });
+
+// // Logout
+// router.post('/logout', (req, res) => {
+//   if (req.session.loggedIn) {
+//     req.session.destroy(() => {
+//       res.status(204).end();
+//     });
+//   } else {
+//     res.status(404).end();
+//   }
+// });
+
+module.exports = router;
